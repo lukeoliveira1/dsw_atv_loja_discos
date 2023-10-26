7c46cfd19f90729a55e942eb018c688a62992cb0
Lucas <lucasoliveirars22@gmail.com>
2023-10-26 10:10:44 -0300

    editar disco

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from core.views import list_disks, detail_disk, create_disk
+from core.views import list_disks, detail_disk, create_disk, edit_disk
 
 app_name = "core"
 
 urlpatterns = [
     path('', list_disks, name='list-disks'),
     path('create-disk', create_disk, name='create-disk'),
+    path('edit-disk/<int:disk_id>', edit_disk, name="edit-disk"),
     path('disk/<int:disk_id>/', detail_disk, name='detail-disk'),
 ]
--- core/views.py
+++ core/views.py
@@ -14,22 +14,32 @@ def detail_disk(request, disk_id):
     return render(request, 'detail_disk.html', {'disk': disk})
 
 def create_disk(request):
+    context = {'acao': 'Cadastrar'}
     if request.method == "POST":
         form = DiskForm(request.POST)
         if form.is_valid():
-            disk = Disk()
-            disk.name = form.cleaned_data["name"]
-            disk.description = form.cleaned_data["description"]
-            disk.phonographic_seal = form.cleaned_data["phonographic_seal"]
-            disk.year = form.cleaned_data["year"]
-            disk.country = form.cleaned_data["country"]
-            disk.gender = form.cleaned_data["gender"]
-            disk.quantity = form.cleaned_data["quantity"]
-            disk.save()
+            form.save()
             return HttpResponseRedirect(reverse('core:list-disks'))
         else:
-            return render(request, 'core/templates/form_create_disk.html', {'form': form})
+            context['form'] = form
+            return render(request, 'disks/form_disk.html', context)
     else:
-        form = DiskForm()
-        return render(request, 'form_create_disk.html', {'form': form})
+        context['form'] = DiskForm()
+        return render(request, 'disks/form_disk.html', context)
     
+def edit_disk(request, disk_id):
+    context = {'acao': 'Editar'}
+    if request.POST:
+        form = DiskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('core:list-disks'))
+        else:
+            context['form'] = form
+            return render(request, 'disks/form_disk.html', context)
+    else:
+        disk = Disk.objects.get(pk=disk_id)
+        form = DiskForm(instance=disk)
+        context['form'] = form
+
+        return render(request, 'disks/form_disk.html', context)
